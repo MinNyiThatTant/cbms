c02f020a4b5684fa694299d99172e07b721da951
MinNyiThatTant <minnyithattant@gmail.com>
2025-07-21 18:17:50 +0630

    doctor's session

--- admin/add-session.php
+++ admin/add-session.php
@@ -1,30 +1,38 @@
 <?php
 
-    session_start();
+session_start();
 
-    if(isset($_SESSION["user"])){
-        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='a'){
-            header("location: ../login.php");
-        }
-
-    }else{
+if (isset($_SESSION["user"])) {
+    if (($_SESSION["user"]) == "" or $_SESSION['usertype'] != 'a') {
         header("location: ../login.php");
+        exit(); // It's a good practice to call exit after a header redirect
     }
-    
-    
-    if($_POST){
-        //import database
-        include("../connection.php");
-        $title=$_POST["title"];
-        $docid=$_POST["docid"];
-        $nop=$_POST["nop"];
-        $date=$_POST["date"];
-        $time=$_POST["time"];
-        $sql="insert into schedule (docid,title,scheduledate,scheduletime,nop) values ($docid,'$title','$date','$time',$nop);";
-        $result= $database->query($sql);
-        header("location: schedule.php?action=session-added&title=$title");
-        
-    }
+} else {
+    header("location: ../login.php");
+    exit(); // It's a good practice to call exit after a header redirect
+}
 
+if ($_POST) {
+    // Import database
+    include("../connection.php");
+
+    // Escape the input values to prevent SQL injection
+    $title = mysqli_real_escape_string($database, $_POST["title"]);
+    $docid = mysqli_real_escape_string($database, $_POST["docid"]);
+    $nop = mysqli_real_escape_string($database, $_POST["nop"]);
+    $date = mysqli_real_escape_string($database, $_POST["date"]);
+    $time = mysqli_real_escape_string($database, $_POST["time"]);
+
+    // Construct the SQL query
+    $sql = "INSERT INTO schedule (docid, title, scheduledate, scheduletime, nop) VALUES ('$docid', '$title', '$date', '$time', '$nop')";
+
+    // Execute the query
+    if ($database->query($sql) === TRUE) {
+        header("location: schedule.php?action=session-added&title=$title");
+        exit(); // It's a good practice to call exit after a header redirect
+    } else {
+        echo "Error: " . $sql . "<br>" . $database->error; // Display error if query fails
+    }
+}
 
 ?>
